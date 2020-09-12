ed0afdc56afffd73a971c6055fd438e325c123a4
HermelaM <hermela.bb@gmail.com>
2020-09-12 00:37:59 -0700

    correction

--- Surfs_Up/app.py
+++ Surfs_Up/app.py
@@ -24,9 +24,6 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -50,6 +47,8 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     """Return the precipitation data for the last year"""
     # Calculate the date 1 year ago from last date in database
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
@@ -57,44 +56,53 @@ def precipitation():
     # Query for the date and precipitation for the last year
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
-
+    session.close()
     # Dict with date as the key and prcp as the value
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of stations."""
-    results = session.query(Station.station).all()
+    # Create our session (link) from Python to the DB
+        session = Session(engine)
+        # Return a JSON List of Stations From the Dataset
+        stations = session.query(Station.station, Station.name).all()
 
-    # Unravel results into a 1D array and convert to a list
-    stations = list(np.ravel(results))
-    return jsonify(stations)
+        session.close()
+        # Convert List of Tuples Into Normal List
+        station_list = list(stations)
+        # Return JSON List of Stations from the Dataset
+        return jsonify(station_list)
+    
 
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     """Return the temperature observations (tobs) for previous year."""
     # Calculate the date 1 year ago from last date in database
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     # Query the primary station for all tobs from the last year
-    results = session.query(Measurement.tobs).\
-        filter(Measurement.station == 'USC00519281').\
+    tobs = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00514830').\
         filter(Measurement.date >= prev_year).all()
 
-    # Unravel results into a 1D array and convert to a list
-    temps = list(np.ravel(results))
+    session.close()
+    #  Convert List of Tuples Into Normal List
+    tobs_df = list(np.ravel(tobs))
 
-    # Return the results
-    return jsonify(temps)
+    # Return the result
+    return jsonify(tobs_df)
 
 
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
     """Return TMIN, TAVG, TMAX."""
-
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     # Select statement
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
@@ -105,7 +113,7 @@ def stats(start=None, end=None):
         # Unravel results into a 1D array and convert to a list
         temps = list(np.ravel(results))
         return jsonify(temps)
-
+    session.close()
     # calculate TMIN, TAVG, TMAX with start and stop
     results = session.query(*sel).\
         filter(Measurement.date >= start).\
